3af9e9914381bb077ba56766c4efc7e40ef8e522
javialroro <102406440+javialroro@users.noreply.github.com>
2024-04-13 11:53:12 -0600

    fdasd

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const {Database} = require('./db.js');
 const {Database2} = require('./db2.js');
